8a2bc489d831d71a1a0d6775f77e4e60cd471489
cmsong <dlcmaldmlzldxkdlrj@gmail.com>
2023-01-13 23:02:04 +0900

    migrate to cobra

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/itsmekingtiger/mvdup
 go 1.18
 
 require (
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	go.etcd.io/bbolt v1.3.6 // indirect
 	golang.org/x/sys v0.0.0-20200923182605-d9f96fdee20d // indirect
 )
--- main.go
+++ main.go
@@ -1,81 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"crypto/sha256"
-	"database/sql"
-	"encoding/hex"
-	"fmt"
-	"log"
-	"os"
-)
-
-var (
-	read_path = "C:\\Users\\baltha\\Downloads\\Video"
-	out_path  = ""
-)
-
-var (
-	DBFile = "mvdup.bolt"
-	Bucket = []byte("HashValues")
-)
+import "github.com/itsmekingtiger/mvdup/cmd"
 
 func main() {
-
-	// Open database
-	db, err := sql.Open("sqlite3", "./foo.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	if createTable(db) != nil {
-		log.Fatal(err)
-	}
-
-	// Read files
-	files, err := readDir(read_path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Hash files
-	for _, file := range files {
-		hash := getHashOfFileAsStream(file)
-		fmt.Println(file, hex.EncodeToString(hash))
-
-		entry := FileEntry{
-			name: file,
-			size: 0, // FIXME:
-			hash: hex.EncodeToString(hash),
-		}
-
-		if insert(db, entry) != nil {
-			log.Fatal(err)
-		}
-	}
-	fmt.Printf("found %d files\n", len(files))
-
-}
-
-func getHashOfFileAsStream(filep string) []byte {
-	file, err := os.Open(filep)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	// stat, _ := file.Stat()
-	// stat.Size()
-
-	sha256 := sha256.New()
-	for {
-		bytes := make([]byte, 4096)
-		n, err := file.Read(bytes)
-		if n == 0 || err != nil {
-			break
-		}
-		sha256.Write(bytes[0:n])
-	}
-
-	return sha256.Sum(nil)
+	cmd.Execute()
 }
